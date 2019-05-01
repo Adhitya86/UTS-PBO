14d5a2ad2f4f742b06d9de760928b93f065562dc
Acer Swift 3 <Acer Swift 3@Kuroashi>
2019-05-01 22:24:12 +0700

    finish

--- RemidiPbo/src/Driver.java
+++ RemidiPbo/src/Driver.java
@@ -10,6 +10,13 @@
  */
 public class Driver {
     public static void main(String[] args) {
+        Intel intel1 = new Intel(2);
+        AMD amd1 = new AMD (3);
+        komputer komputer1 = new komputer(intel1);
         
+            komputer1.cetakInfo();
+            komputer1.memasang(intel1);
+            komputer1.mencabut(amd1);
+            komputer1.cetakInfo();
     }
 }
